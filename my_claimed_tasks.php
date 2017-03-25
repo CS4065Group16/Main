789b12df5145fedf779de6333e31061d250903a4
neiltimothy <0668818@studentmail.ul.ie>
2017-03-25 00:23:45 +0000

    Add files via upload

--- my_claimed_tasks.php
+++ my_claimed_tasks.php
@@ -9,7 +9,7 @@
 
       
       
-    $query = "UPDATE task SET task_statues = '1' WHERE task_id ={$the_task_id} ";
+    $query = "UPDATE task SET task_statues = '2' WHERE task_id ={$the_task_id} ";
                                     
       
     $update_query = mysqli_query($connection, $query);
@@ -68,13 +68,10 @@
    
    
    <html>
-    
+<h4>Tasks you accepted that are waiting approval by author</h4>    
 <table class = "table table-bordered table-hover">
                             <thead>
                                 <tr>
-                                    <th>Task Id</th>
-                                    <th>Creator Id</th>
-                                    <th>Claim Id</th>
                                     <th>Task Title</th>
                                     <th>Task Type</th>
                                     <th>Task Desc</th>
@@ -84,7 +81,7 @@
                                     <th>File Format</th>
                                     <th>Claim Dadline</th>
                                     <th>Submission Deadline</th>
-                                    <th>Flagged Status</th>
+                                    
                                     <th>Task Tags</th>
                                 </tr>
                             </thead>
@@ -101,7 +98,7 @@
                                   
                                   
     
-                                $query = "SELECT * FROM task WHERE claim_id = '{$userID}' ";
+                                $query = "SELECT * FROM task WHERE claim_id = '{$userID}' and task_statues = '2' ";
                                 $select_task = mysqli_query($connection, $query);
                         
                                 
@@ -139,9 +136,9 @@
                                             echo "<td>{$flagged_status}</td>";
                                             echo "<td>{$task_tags}</td>";
                                             /*Dividing parameters with p_id*/        
-                                             echo "<td><a href ='tasks.php?source=edit_task&p_id={$task_id}'>Edit</a></td>";
+                                           
                                             /*Delete has to send the task id with it*/     
-                                            echo "<td><a href ='tasks.php?delete={$task_id}'>Delete</a></td>";
+                                            echo "<td><a href ='tasks.php?source=view&withdraw={$task_id}'>Withdraw offer</a></td>";
                                            
                                             
                                         echo "</tr>";
@@ -156,20 +153,121 @@
                         
                         </tbody>
                         </table>
-                        
-                        
-                        <?php
+                      
+                       
 
-                        /*if(isset($_GET['delete']))
-                            
-                           $the_post_id = $_GET['delete'];
-                            
-                            $query = "DELETE FROM task WHERE task_id = {$the_post_id} ";
-                            $delete_query = mysqli_query($connection, $query);
-                            */
+
+                        
+</html>
+
+
+
+
+<html>
+<h4>Task you claimed (in progress) </h4>    
+<table class = "table table-bordered table-hover">
+                            <thead>
+                                <tr>
+<th>Task Title</th>
+                                    <th>Task Type</th>
+                                    <th>Task Desc</th>
+                                    <th>Task Subject</th>
+                                    <th>Page Count</th>
+                                    <th>Word Count</th>
+                                    <th>File Format</th>
+                                    <th>Claim Dadline</th>
+                                    <th>Submission Deadline</th>
+                                </tr>
+                            </thead>
+                              <tbody>
+                               <?php
                                 
+                                if(isset($_SESSION['userID'])) {
+        
+                                $userID = $_SESSION['userID'];
+
+                               /* $query ="SELECT * FROM user WHERE user_name = '{$username}' ";*/
+
+                                  
+                                  
+                                  
+    
+                                $query = "SELECT * FROM task WHERE claim_id = '{$userID}' and task_statues = '3' ";
+                                $select_task = mysqli_query($connection, $query);
+                        
+                                
+                                 while  ($row = mysqli_fetch_assoc($select_task)) {
+                                        $task_id                =       $row['task_id'];
+                                        $creator_id             =       $row['creator_id'];
+                                        $claim_id               =       $row['claim_id'];
+                                        $task_title             =       $row['task_title'];
+                                        $task_type              =       $row['task_type'];
+                                        $task_desc              =       $row['task_desc'];
+                                        $task_subject           =       $row['task_subject'];
+                                        $page_count             =       $row['page_count'];
+                                        $word_count             =       $row['word_count'];
+                                        $file_format            =       $row['file_format'];
+                                        $claim_deadline         =       $row['claim_deadline'];
+                                        $submission_deadline    =       $row['submission_deadline'];
+                                        $flagged_status         =       $row['flagged_status'];
+                                        $task_tags              =       $row['task_tags'];
+                                        
+                                        //Need to echo all above in a one row each time we have a task
+                                     
+                                        echo "<tr>";
+                                            echo "<td>{$task_id}</td>";
+                                            echo "<td>{$creator_id}</td>";
+                                            echo "<td>{$claim_id}</td>";    
+                                            echo "<td>{$task_title}</td>";
+                                            echo "<td>{$task_type}</td>"; 
+                                            echo "<td>{$task_desc}</td>";
+                                            echo "<td>{$task_subject}</td>"; 
+                                            echo "<td>{$page_count}</td>";
+                                            echo "<td>{$word_count}</td>";     
+                                            echo "<td>{$file_format}</td>";
+                                            echo "<td>{$claim_deadline}</td>"; 
+                                            echo "<td>{$submission_deadline}</td>";
+                                            echo "<td>{$flagged_status}</td>";
+                                            echo "<td>{$task_tags}</td>";
+                                            /*Dividing parameters with p_id*/        
+                                            echo "<td><a href ='tasks.php?source=view&withdraw={$task_id}'>Withdraw offer</a></td>";
+                                           
+                                            
+                                        echo "</tr>";
+    
+                                 }
+                                    
+                                }
+                                  
+                                ?>
+                                  
+                                   
+                        
+                        </tbody>
+                        </table>
+                          <?php
+
+                           if(isset($_GET['withdraw'])) {
+                            
+                            $the_task_id_for_decline = $_GET['withdraw'];
+                            
+                            $query = "UPDATE task SET task_statues = '1' WHERE task_id = {$the_task_id_for_decline} ";
+                            $decline_query = mysqli_query($connection, $query);
+                            
+
+                            if(!$decline_query){
+                                die ('Query failed' . mysqli_error($connection));
+                                                }
+                                      
+                                  }
+
+
+                    
                             
                             ?>
+                        
+                        
+                        
 
 
                         
---
+++ post.php
@@ -0,0 +1,183 @@
+
+<?php include "partials/db.php"; ?>
+<?php include "partials/header.php"; ?>
+
+
+
+    <!-- Navigation -->
+    <?php include "partials/navigation.php"; ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            
+                
+                
+                <div class="col-md-8">
+                
+                
+             
+                
+                <?php
+                    
+                    
+                     /*<!--Catching the id being sent so we can identify the post in question-->*/
+                
+                if(isset($_GET['p_id'])) {
+                   
+                   $the_task_id = $_GET['p_id'];
+                    
+                  
+               
+            
+                    
+                $query = "SELECT * from task WHERE task_id = {$the_task_id} ";
+                    
+                $select_all_task = mysqli_query($connection, $query);
+                        
+                while ($row = mysqli_fetch_assoc($select_all_task)) {
+                        $task_id                =       $row['task_id'];    
+                        $task_title             =       $row['task_title'];
+                        $task_type              =       $row['task_type'];
+                        $task_desc              =       $row['task_desc'];
+                        $task_subject           =       $row['task_subject'];
+                        $page_count             =       $row['page_count'];
+                        $word_count             =       $row['word_count'];
+                        $claim_deadline         =       $row['claim_deadline'];
+                        $submission_deadline    =       $row['submission_deadline'];
+                   
+                    ?>
+                    
+                     
+                       <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $task_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $task_type ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> created on <?php echo $submission_deadline ?></p>
+                <hr>
+                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <hr>
+                <p><?php echo $claim_deadline ?> </p>
+               
+             
+                     <a class="btn btn-primary" href="admin/tasks.php?source=view&t_id=<?php echo $task_id; ?>" name="claim"> Claim Task <span class="glyphicon glyphicon-chevron-right"></span></a>
+                     
+                      <a class="btn btn-warning" href="index.php?t_id=<?php echo $task_id; ?>" name="flagg"> Flag Task <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <hr>
+                    
+                    
+                    
+                     
+                    
+                        
+                    <!--turning on PHP again for closing loop -->
+                   <?php }?>
+                    
+                <?php }?>
+                    
+                    
+                     <!-- Blog Comments -->
+
+                <!-- Comments Form -->
+                <div class="well">
+                    <h4>Leave a Comment:</h4>
+                    <form role="form">
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                     
+
+                    </form>
+                </div>
+                
+              
+                   
+                
+                
+                
+                
+                
+                
+                
+                <hr>
+
+                <!-- Posted Comments -->
+                
+                
+
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <!-- Nested Comment -->
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading">Nested Start Bootstrap
+                                    <small>August 25, 2014 at 9:30 PM</small>
+                                </h4>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            </div>
+                        </div>
+                        <!-- End Nested Comment -->
+                    </div>
+                </div>
+                     
+
+                    
+                
+                
+
+              
+                
+                
+
+
+            </div>
+
+           
+           
+           
+            <!-- Sidebar Widgets Column -->
+            
+            <?php include "partials/sideBar.php"; ?>   
+                
+              
+
+        </div>
+        <!-- /.row -->
+
+       <?php include "partials/footer.php"; ?>
+
+        
