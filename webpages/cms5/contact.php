65508ac79ba0bd338918df109b3722ea1d0623b9
Paul McCarthy <16137604@studentmail.ul.ie>
2017-04-09 14:13:02 +0100

    contact page
    This contact page is generated using a type form. Looks good, however could be more worth while to develop our own one

---
+++ webpages/cms5/contact.php
@@ -0,0 +1,6 @@
+<?php  include "partials/header.php"; ?>
+<?php  include "admin/functions.php"; ?>
+<?php  include "partials/navigation.php"; ?>
+
+<div class="typeform-widget" data-url="https://paulmccarthy.typeform.com/to/xfmPyL" style="width: 100%; height: 500px;"></div><script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id="typef_orm",b="https://s3-eu-west-1.amazonaws.com/share.typeform.com/";if(!gi.call(d,id)){js=ce.call(d,"script");js.id=id;js.src=b+"widget.js";q=gt.call(d,"script")[0];q.parentNode.insertBefore(js,q)}})()</script>
+<?php include "partials/footer.php"?>
