c08e901e48bc979b756153d8dfed9e910c88ba0f
richyroc <10004172@studentmail.ul.ie>
2017-04-10 04:51:31 +0100

    Add files via upload

---
+++ webpages/richy/play_it_by_peer_db.sql
@@ -0,0 +1,419 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2017 at 05:39 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `play_it_by_peer_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned_users`
+--
+
+CREATE TABLE `banned_users` (
+  `banned_id` int(10) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `category` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category`) VALUES
+(1, 'Finance'),
+(2, 'Programing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `claimed_tasks`
+--
+
+CREATE TABLE `claimed_tasks` (
+  `claimed_id` int(10) UNSIGNED NOT NULL,
+  `task_id` int(10) UNSIGNED NOT NULL,
+  `student_id` int(10) UNSIGNED NOT NULL,
+  `claimed_date` datetime NOT NULL,
+  `claimed_expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clicks`
+--
+
+CREATE TABLE `clicks` (
+  `click_id` bigint(100) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `tag_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `document`
+--
+
+CREATE TABLE `document` (
+  `document_id` int(10) UNSIGNED NOT NULL,
+  `document_loc` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `document`
+--
+
+INSERT INTO `document` (`document_id`, `document_loc`) VALUES
+(1, 'www.x.com'),
+(2, '1'),
+(3, '20170130-0340_CS4416_-_Tutorial_1.pdf'),
+(4, 'user-login-page-with-icons-vector-5076899.jpg'),
+(5, 'user-login-page-with-icons-vector-5076899.jpg'),
+(6, '20170130-0340_CS4416_-_Tutorial_1.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flagged_task`
+--
+
+CREATE TABLE `flagged_task` (
+  `flag_id` int(10) UNSIGNED NOT NULL,
+  `task_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `flag_comment` text NOT NULL,
+  `flag_date` datetime NOT NULL,
+  `flag_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `tag_id` int(10) UNSIGNED NOT NULL,
+  `tag` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`tag_id`, `tag`) VALUES
+(1, 'Maths'),
+(2, 'computer'),
+(3, 'project-report');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(10) UNSIGNED NOT NULL,
+  `creator_id` int(10) UNSIGNED NOT NULL DEFAULT '1',
+  `task_title` varchar(128) NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `file_id` int(10) UNSIGNED NOT NULL,
+  `task_description` text,
+  `word_count` int(11) NOT NULL,
+  `page_count` int(11) NOT NULL,
+  `file_format` varchar(32) NOT NULL,
+  `document` varchar(64) NOT NULL,
+  `created_date` date NOT NULL,
+  `expiration_date` date NOT NULL,
+  `status_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `creator_id`, `task_title`, `category_id`, `file_id`, `task_description`, `word_count`, `page_count`, `file_format`, `document`, `created_date`, `expiration_date`, `status_id`) VALUES
+(12, 5, 'test', 1, 1, 'test', 1000000, 1200, '', '', '2017-04-10', '0000-00-00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_intermediate_tag`
+--
+
+CREATE TABLE `task_intermediate_tag` (
+  `tag_id` int(10) UNSIGNED NOT NULL,
+  `task_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_status`
+--
+
+CREATE TABLE `task_status` (
+  `status_id` int(10) UNSIGNED NOT NULL,
+  `status` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `task_status`
+--
+
+INSERT INTO `task_status` (`status_id`, `status`) VALUES
+(1, 'open'),
+(2, 'claimed'),
+(3, 'approved'),
+(4, 'unclaimed'),
+(5, 'finished');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `staff_id` int(100) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `reputation` int(10) DEFAULT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `staff_id`, `user_name`, `first_name`, `last_name`, `email`, `reputation`, `category_id`, `password`) VALUES
+(5, 0, 'superman', '', '', 'ddqd', 60, 0, 'superman'),
+(6, 0, 'dwqdwq', 'dq', 'dqw', 'qd', 0, 0, 'wqd'),
+(7, 0, 'dwqdwq', 'dq', 'dqw', 'qd', 0, 0, 'wqd'),
+(8, 0, 'nfrtnhn', 'nrtn', 'rtn', 'rtn', 0, 0, 'nrtntr'),
+(9, 0, 'ewd', 'qd', 'qwd', 'dqw', 0, 0, 'dqwd'),
+(10, 0, 'dqw', 'dq', 'dqw', 'qwd', 0, 0, 'qdqw'),
+(11, 0, 'ed', 'e', 'ded', 'ed', 0, 0, 'aed'),
+(12, 0, 'ef', 'ef', 'e', 'ewe', 0, 2, 'fewef'),
+(13, 0, 'de', 'e', 'de', 'de', 0, 1, 'deded');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banned_users`
+--
+ALTER TABLE `banned_users`
+  ADD PRIMARY KEY (`banned_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indexes for table `claimed_tasks`
+--
+ALTER TABLE `claimed_tasks`
+  ADD PRIMARY KEY (`claimed_id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `user_id` (`student_id`);
+
+--
+-- Indexes for table `clicks`
+--
+ALTER TABLE `clicks`
+  ADD PRIMARY KEY (`click_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `document`
+--
+ALTER TABLE `document`
+  ADD PRIMARY KEY (`document_id`);
+
+--
+-- Indexes for table `flagged_task`
+--
+ALTER TABLE `flagged_task`
+  ADD PRIMARY KEY (`flag_id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`creator_id`),
+  ADD KEY `file_id` (`file_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `creator_id` (`creator_id`),
+  ADD KEY `status_id` (`status_id`);
+
+--
+-- Indexes for table `task_intermediate_tag`
+--
+ALTER TABLE `task_intermediate_tag`
+  ADD PRIMARY KEY (`tag_id`,`task_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `task_status`
+--
+ALTER TABLE `task_status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banned_users`
+--
+ALTER TABLE `banned_users`
+  MODIFY `banned_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `claimed_tasks`
+--
+ALTER TABLE `claimed_tasks`
+  MODIFY `claimed_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `clicks`
+--
+ALTER TABLE `clicks`
+  MODIFY `click_id` bigint(100) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `document`
+--
+ALTER TABLE `document`
+  MODIFY `document_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `flagged_task`
+--
+ALTER TABLE `flagged_task`
+  MODIFY `flag_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tag_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `task_status`
+--
+ALTER TABLE `task_status`
+  MODIFY `status_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `banned_users`
+--
+ALTER TABLE `banned_users`
+  ADD CONSTRAINT `banned_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `claimed_tasks`
+--
+ALTER TABLE `claimed_tasks`
+  ADD CONSTRAINT `claimed_tasks_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `claimed_tasks_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `clicks`
+--
+ALTER TABLE `clicks`
+  ADD CONSTRAINT `clicks_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `clicks_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `flagged_task`
+--
+ALTER TABLE `flagged_task`
+  ADD CONSTRAINT `flagged_task_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `flagged_task_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_5` FOREIGN KEY (`status_id`) REFERENCES `task_status` (`status_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasks_ibfk_7` FOREIGN KEY (`creator_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasks_ibfk_8` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `task_intermediate_tag`
+--
+ALTER TABLE `task_intermediate_tag`
+  ADD CONSTRAINT `task_intermediate_tag_ibfk_4` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `task_intermediate_tag_ibfk_5` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
